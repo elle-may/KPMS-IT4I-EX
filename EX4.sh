d6db8d6af9b63da94d9c83a830dd406c01bc6845
Elle Downer <eleanor.downer96@googlemail.com>
2022-03-20 21:40:24 +0100

    4 cores only

--- EX4.sh
+++ EX4.sh
@@ -4,12 +4,5 @@ pwd
 module load R
 echo "loaded R"
 
-time Rscript EX4.r
-time Rscript EX4.r 1
-time Rscript EX4.r 2
+
 time Rscript EX4.r 4
-time Rscript EX4.r 8
-time Rscript EX4.r 16
-time Rscript EX4.r 32
-time Rscript EX4.r 64
-time Rscript EX4.r 128
