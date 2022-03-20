159212d4295d4d02833ed3922cfdcadff41fb7ce
Elle Downer <eleanor.downer96@googlemail.com>
2022-03-20 18:10:05 +0100

    added additional parellization

--- EX4.R
+++ EX4.R
@@ -3,13 +3,19 @@ library(parallel)
 data(LetterRecognition, package = "mlbench")
 set.seed(seed = 123, "L'Ecuyer-CMRG")
 
+# number rows: 20000
 n = nrow(LetterRecognition)
+
+# split into train and test set, ratio 0.2:0.8
 n_test = floor(0.2 * n)
 i_test = sample.int(n, n_test)
 train = LetterRecognition[-i_test, ]
 test = LetterRecognition[i_test, ]
 
-ntree = 200
+# obtain number of cores
+nc = as.numeric(commandArgs(TRUE)[1])
+cat("Running with", nc, "cores\n")
+ntree = lapply(splitIndices(200, nc), length)
 nfolds = 10
 mtry_val = 1:(ncol(train) - 1)
 folds = sample( rep_len(1:nfolds, nrow(train)), nrow(train) )
@@ -24,8 +30,6 @@ fold_err = function(i, cv_pars, folds, train) {
   sum(pred != train$lettr[fold])
 }
 
-nc = as.numeric(commandArgs(TRUE)[1])
-cat("Running with", nc, "cores\n")
 system.time({
   cv_err = parallel::mclapply(1:nrow(cv_pars), fold_err, cv_pars, folds = folds,
                               train = train, mc.cores = nc) 
@@ -33,13 +37,33 @@ system.time({
 })
 pdf(paste0("rf_cv_mc", nc, ".pdf")); plot(mtry_val, err/(n - n_test)); dev.off()
 
-rf.all = randomForest(lettr ~ ., train, ntree = ntree)
-pred = predict(rf.all, test)
+#ntree = lapply(splitIndices(500, nc), length)
+rf = function(x) randomForest(lettr ~ ., train, ntree=ntree)
+rf.out = mclapply(ntree, rf, mc.cores = nc)
+rf.all = do.call(combine, rf.out)
+
+crows = splitIndices(nrow(test), nc) 
+rfp = function(x) as.vector(predict(rf.all, test[x, ])) 
+cpred = mclapply(crows, rfp, mc.cores = nc) 
+pred = do.call(c, cpred)
 correct = sum(pred == test$lettr)
 
+#rf.all = randomForest(lettr ~ ., train, ntree = ntree)
+#pred = predict(rf.all, test)
+
 mtry = mtry_val[which.min(err)]
-rf.all = randomForest(lettr ~ ., train, ntree = ntree, mtry = mtry)
-pred_cv = predict(rf.all, test)
-correct_cv = sum(pred_cv == test$lettr)
-cat("Proportion Correct: ", correct/n_test, "(mtry = ", floor((ncol(test) - 1)/3),
-    ") with cv:", correct_cv/n_test, "(mtry = ", mtry, ")\n", sep = "")
+rf2 = function(x) randomForest(lettr ~ ., train, ntree=ntree, mtry = mtry)
+rf2.out = mclapply(ntree, rf2, mc.cores = nc)
+rf2.all = do.call(combine, rf2.out)
+
+#crows = splitIndices(nrow(test), nc) 
+rfp2 = function(x) as.vector(predict(rf2.all, test[x, ])) 
+cpred2 = mclapply(crows, rfp2, mc.cores = nc) 
+pred2 = do.call(c, cpred2)
+correct2 = sum(pred2 == test$lettr)
+
+#rf.all = randomForest(lettr ~ ., train, ntree = ntree, mtry = mtry)
+pred2 = do.call(c, cpred2)
+correct2 = sum(pred2 == test$lettr)
+cat("Proportion Correct: ", correct2/n_test, "(mtry = ", floor((ncol(test) - 1)/3),
+    ") with cv:", correct2/n_test, "(mtry = ", mtry, ")\n", sep = "")
