bedaf730804a49d3c98fbc38e5254f38c88781da
Elle Downer <eleanor.downer96@googlemail.com>
2022-04-18 17:06:11 +0200

    changed paths

--- EX8.R
+++ EX8.R
@@ -88,8 +88,8 @@ model_report = function(models, kplot = 0) {
 
 library(parallel)
 library(ggplot2)
-source("../mnist/mnist_read.R")
-source("../code/flexiblas_setup.r")
+source("..\\KPMS-IT4I-EX\\mnist\\mnist_read.R")
+source("..\\KPMS-IT4I-EX\\code\\flexiblas_setup.r")
 blas_threads = as.numeric(commandArgs(TRUE)[2])
 fork_cores = as.numeric(commandArgs(TRUE)[3])
 setback("OPENBLAS")
