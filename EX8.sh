404ce405128bead23e01e1d646cbc91b2eb59e99
Elle Downer <eleanor.downer96@googlemail.com>
2022-04-19 14:38:39 +0200

    args debug

--- EX8.sh
+++ EX8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N mnist_svd_cv
-#PBS -l select=4:mpiprocs=8
+#PBS -l select=2:mpiprocs=8
 #PBS -q qexp
 #PBS -e EX8.e
 #PBS -o EX8.o
@@ -18,4 +18,4 @@ export RDMAV_FORK_SAFE=1
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
 ## --args blas fork
-time mpirun --map-by ppr:2:node Rscript EX8.R
+time mpirun --map-by ppr:4:node Rscript EX8.R
