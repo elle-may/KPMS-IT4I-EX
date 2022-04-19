7aab30165591187611a0cb1e1fd9f1eaa24a496f
Elle Downer <eleanor.downer96@googlemail.com>
2022-04-19 14:29:04 +0200

    ARGS DEBUG

--- EX8.R
+++ EX8.R
@@ -4,13 +4,8 @@ library(pbdMPI)
 source("../KPMS-IT4I-EX/mnist/mnist_read.R")
 source("../KPMS-IT4I-EX/code/flexiblas_setup.r")
 
-args <- commandArgs()
+args <- commandArgs(TRUE)
 print(args)
 
-
-blas_threads = as.numeric(commandArgs(TRUE)[2])
-fork_cores = as.numeric(commandArgs(TRUE)[4])
-setback("OPENBLAS")
-setthreads(blas_threads)
 
 
