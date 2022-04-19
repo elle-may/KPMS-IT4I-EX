9170374e1998c90d6307352b611b307b169cd918
Elle Downer <eleanor.downer96@googlemail.com>
2022-04-19 14:19:50 +0200

    changed args

--- EX8.R
+++ EX8.R
@@ -4,7 +4,7 @@ library(pbdMPI)
 source("../KPMS-IT4I-EX/mnist/mnist_read.R")
 source("../KPMS-IT4I-EX/code/flexiblas_setup.r")
 blas_threads = as.numeric(commandArgs(TRUE)[2])
-fork_cores = as.numeric(commandArgs(TRUE)[3])
+fork_cores = as.numeric(commandArgs(TRUE)[4])
 setback("OPENBLAS")
 setthreads(blas_threads)
 
