42b00ebcfdfae0a1a6a230fcb73f1bb934db0ea6
Elle Downer <eleanor.downer96@googlemail.com>
2022-04-19 14:26:38 +0200

    debug args

--- EX8.sh
+++ EX8.sh
@@ -18,4 +18,4 @@ export RDMAV_FORK_SAFE=1
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
 ## --args blas fork
-time mpirun --map-by ppr:1:node Rscript EX8.R --args 4
+time mpirun --map-by ppr:2:node Rscript EX8.R --args 4
