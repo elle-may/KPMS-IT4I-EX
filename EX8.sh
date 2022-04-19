b440f786b7d0b3aa37923f80c73ce5cf999472e3
Elle Downer <eleanor.downer96@googlemail.com>
2022-04-19 14:04:37 +0200

    added exports

--- EX8.sh
+++ EX8.sh
@@ -11,6 +11,10 @@ pwd
 module load R
 echo "loaded R"
 
+## prevent warning when fork is used with MPI
+export OMPI_MCA_mpi_warn_on_fork=0
+export RDMAV_FORK_SAFE=1
+
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
 ## --args blas fork
