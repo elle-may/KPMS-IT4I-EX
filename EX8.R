844f04e8a69b35d16eb4b00d99a137bbfef30f3d
Elle Downer <eleanor.downer96@googlemail.com>
2022-04-20 20:36:48 +0200

    debug

--- EX8.R
+++ EX8.R
@@ -106,7 +106,7 @@ pars = seq(80.0, 95, .2)## par values to fit
 my_test_rows = comm.chunk(nrow(test), form = "vector")
 my_train_rows = comm.chunk(nrow(train), form = "vector")
 
-folds = sample( rep_len(1:nfolds, nrow(my_train_rows)), nrow(my_train_rows ), size = my_rank + 1 ) ## random folds
+folds = sample(1:nfolds, nrow(my_train_rows), nrow(my_train_rows ), size = my_rank + 1 ) ## random folds
 cv = expand.grid(par = pars, fold = 1:nfolds)  ## all combinations
 
 ## function for parameter combination i
