7df6d16b9245cb1cbb8590e45edf12d66756d7a8
Elle Downer <eleanor.downer96@googlemail.com>
2022-04-21 12:35:21 +0200

    debug

--- EX8.R
+++ EX8.R
@@ -128,7 +128,7 @@ fold_err = function(i, cv, folds, train) {
 }
 
 ## apply fold_err() over parameter combinations
-my_cv_err = lapply(1:my_train_rows, fold_err, cv = my_index, folds = nfolds, train = my_train_rows)
+my_cv_err = lapply(1:pars, fold_err, cv = my_index, folds = nfolds, train = my_train_rows)
 
 ## sum fold errors for each parameter value
 cv_err = allgather(my_cv_err)`  
